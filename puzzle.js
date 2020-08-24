a4c88f2072fb847377adc8b901246802d653bbc4
Nick Thorpe <nicholas_thorpe@mhu.edu>
2020-08-24 12:52:42 -0400

    Added starter Javascript

---
+++ puzzle.js
@@ -0,0 +1,22 @@
+var images = new Array();
+
+for (let i = 0; i < 9; i++)
+    images.push("images/pokemon" + ".png");
+
+var clicks = 0;
+
+/*
+    Documentation
+*/
+function prepareBoard() {
+
+}
+
+/*
+    Documentation
+*/
+function reset() {
+
+}
+
+window.onload = prepareBoard();
