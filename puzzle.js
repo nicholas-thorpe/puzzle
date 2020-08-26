9f3a9d863c7bc7a49c2731f2317e4306d6464b57
Nick Thorpe <nicholas_thorpe@mhu.edu>
2020-08-26 15:34:02 -0400

    Did some things

--- puzzle.js
+++ puzzle.js
@@ -1,42 +1,43 @@
 var clicks = 0;
 var tiles = 9;
 
-var images = new Array();
+var images = new Array(3);
 
-for (let i = 0; i < tiles; i++)
-	images.push("images/cat" + (i + 1) + ".png");
+for (let i = 0; i < 3; i++)
+	images[i] = new Array(3);
 
-images.push("images/blank.png");
+for (let i = 0; i < 3; i++)
+	for (let j = 0; j < 3; j++)
+		images[i][j] = ("images/cat" + ((i * 3) + j + 1) + ".png");
 
 /*
 	Adds the tiled image to the board
 */
 function prepareBoard() {
-	var board;
-	
 	//Check the browser is capable of basic things
 	if (!document.getElementsByTagName)
 		return;
 	if (!document.getElementById)
 		return;
-	if (!document.getElementById('gameboard'))
-		return;
 	
 	//Find the board
-	board = document.getElementById('gameboard');
+	rows = new Array(3)
+	for (let i = 0; i < 3; i++)
+		rows[i] = document.getElementById('row' + i);
 	
 	//Add the images
-	for (let i = 0; i < tiles; i++) {
-		newElement = document.createElement('img');
-		newElement.src = images[i];
-		newElement.id = i;
-		newElement.alt = "cat" + i;
-		
-		newElement.onclick = function(){return move(this);};
-		board.appendChild(newElement);
-	}
-	
-	
+	for (let i = 0; i < 3; i++) {
+		for (let j = 0; j < 3; j++) {
+			newElement = document.createElement('img');
+			newElement.src = images[i][j];
+			newElement.id = (i + j);
+			newElement.alt = "cat" + (i + j);
+			newElement.onclick = function(){return move(this);};
+			
+			var row = row[i];
+			row.appendChild(newElement);
+		}
+	}	
 }
 
 /*
